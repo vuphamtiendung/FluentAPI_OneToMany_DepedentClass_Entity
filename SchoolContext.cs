f04cbc36563a97a94be1436fb50d9dfed70b4d11
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2023-02-19 20:11:59 +0700

    Add project files.

---
+++ SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentAPI_002
+{
+    public class SchoolContextb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"server=Admin; Database=FluentAPIOneToMany; Trusted_Connection=True; Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne<Grade>(s => s.Grades)
+                .WithMany(g => g.Students)
+                .HasForeignKey(c => c.CurrentGradeId);
+        } 
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
